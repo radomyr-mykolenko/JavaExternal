213363b111fc35448cc8cb3c68edbe170c21cb05
Radomyr Mykolenko <radomyr@ukr.net>
2020-03-04 00:15:45 +0200

    localization for numbergame was added

--- NumberGame/src/com/radomyr/GameController.java
+++ NumberGame/src/com/radomyr/GameController.java
@@ -4,23 +4,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class GameController {
     private GameModel model;
     private GameView view;
-
+    private ResourceBundle bundle;
     public GameController(GameModel model, GameView view) {
         this.model = model;
         this.view = view;
     }
 
     public void Game() throws IOException {
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         model.setRandomvalue();
-
+        this.bundle = chooseLang();
         int inputNumber = 0;
         do {
-            view.printMsg(view.INPUT_MESSAGE + model.getMinvalue() + " to " + model.getMaxvalue());
+            //this.bundle = chooseLang();
+            view.printMsg(bundle.getString("INPUT_MESSAGE") + " " + model.getMinvalue() + " - " + model.getMaxvalue());
             try {
                 inputNumber = Integer.parseInt(br.readLine());
             } catch (NumberFormatException e) {
@@ -30,12 +35,12 @@ public class GameController {
             try {
                 if (checkValidRange(inputNumber)) {
                     if (model.checkMatching(inputNumber)) {
-                        view.printMsg(view.WIN_MESSAGE + model.getRandomvalue());
+                        view.printMsg(bundle.getString("WIN_MESSAGE") + model.getRandomvalue());
                     } else {
                         checkNumber(inputNumber);
-                        view.printMsg(view.CONTINUE_MESSAGE);
+                        view.printMsg(bundle.getString("CONTINUE_MESSAGE"));
                     }
-                    System.out.println(view.LIST_OF_ATTEMPTS_MESSAGE + model.getAttempts());
+                    System.out.println(bundle.getString("LIST_OF_ATTEMPTS_MESSAGE") + model.getAttempts());
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -43,7 +48,7 @@ public class GameController {
 
         } while (inputNumber != model.getRandomvalue());
 
-        view.printMsg(view.ATTEMPTS_MESSAGE + model.getAttempts().size());
+        view.printMsg(bundle.getString("ATTEMPTS_MESSAGE") + model.getAttempts().size());
 
     }
 
@@ -51,17 +56,49 @@ public class GameController {
         if (ourNumber > model.getMinvalue() && ourNumber < model.getMaxvalue()) {
             return true;
         } else {
-            System.out.println(view.OUT_OF_RANGE_MESSAGE);
+            System.out.println(bundle.getString("OUT_OF_RANGE_MESSAGE"));
             return false;
         }
     }
 
     public void checkNumber(int number) {
         if (number > model.getRandomvalue()) {
-            view.printMsg(view.LOWER_NUMBER_MESSAGE);
+            view.printMsg(bundle.getString("LOWER_NUMBER_MESSAGE"));
         } else {
-            view.printMsg(view.HIGHER_NUMBER_MESSAGE);
+            view.printMsg(bundle.getString("HIGHER_NUMBER_MESSAGE"));
         }
+    }
+   public ResourceBundle chooseLang() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean marker = true;
+        while (marker) {
+            view.printMsg(view.CHOOSE_THE_LANGUAGE_MESSAGE);
+            int chooseLangRes = Integer.parseInt(br.readLine());
+            switch (chooseLangRes) {
+                case 1: {
+                    marker = false;
+                    String language = "en";
+                    String country = "EN";
+                    Locale lang = new Locale(language,country);
+
+                    bundle = ResourceBundle.getBundle("com.radomyr.resources", lang);
+                    break;
+                }
+                case 2: {
+                    //this.bundle = ResourceBundle.getBundle("resources_ua", new Locale("ua", "UA"));
+                    marker = false;
+                    String language = "ua";
+                    String country = "UA";
+                    Locale lang = new Locale(language,country);
+                    bundle = ResourceBundle.getBundle("com.radomyr.resources", lang);
+                    break;
+                }
+                default: {
+                    view.printMsg(view.TRY_AGAIN_LANG_MESSAGE);
+                }
+            }
+        }
+        return bundle;
     }
 
 }
--- NumberGame/src/com/radomyr/GameView.java
+++ NumberGame/src/com/radomyr/GameView.java
@@ -6,9 +6,11 @@ public class GameView {
     public static final String INPUT_MESSAGE = "Input your integer number in range from ";
     public static final String OUT_OF_RANGE_MESSAGE = "Your number is out of range";
     public static final String CONTINUE_MESSAGE = "Try again";
-    public static final String LIST_OF_ATTEMPTS_MESSAGE = "Your tries";
+    public static final String LIST_OF_ATTEMPTS_MESSAGE = "Your proposed";
     public static final String WIN_MESSAGE = "You are the winner! The number for guessing was: ";
     public static final String ATTEMPTS_MESSAGE = "To guess the number you tried such times: ";
+    public static final String CHOOSE_THE_LANGUAGE_MESSAGE = "Please choose language (1 - eng, 2 - ukr):";
+    public static final String TRY_AGAIN_LANG_MESSAGE = "You inputted something other that is needed, please try again";
 
     public void printMsg(String message) {
         System.out.println(message);
