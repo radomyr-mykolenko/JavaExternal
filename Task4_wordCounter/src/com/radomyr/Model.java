d37048d8370d862b315181e80cc3f918aa5941f0
Radomyr Mykolenko <radomyr@ukr.net>
2020-03-04 13:23:03 +0200

    application works but more useful interface is needed

---
+++ Task4_wordCounter/src/com/radomyr/Model.java
@@ -0,0 +1,10 @@
+package com.radomyr;
+
+import java.util.HashMap;
+
+public class Model {
+    public HashMap<String, HashMap> observer = new HashMap<>();
+    public HashMap<String, Integer> url1Map = new HashMap<>();
+    public HashMap<String, Integer> url2Map = new HashMap<>();
+    public HashMap<String, Integer> url3Map = new HashMap<>();
+}
